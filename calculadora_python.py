72232f319c9449c112979d825a5b926d21e8bdd4
Bernardo Girardi Frigeri <be.frigeri@gmail.com>
2025-07-10 12:21:04 -0300

    Add files via upload

---
+++ calculadora_python.py
@@ -0,0 +1,25 @@
+print('=== Cálculadora ===')
+
+print('=== SOMA ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+soma = a + b
+print('A soma é '+str(soma))
+
+print('=== SUBTRAÇÃO ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+subtracao = a - b
+print('A subtração é '+str(subtracao))
+
+print('=== MULTIPLICAÇÃO ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+multiplicao = a * b
+print('A multiplicação é '+str(multiplicao))
+
+print('=== DIVISÃO ===')
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+divisao = a / b
+print('A divisão é '+str(divisao))
---
+++ python.py
@@ -0,0 +1,14 @@
+#Variáveis Declaradas
+nome = "Bernardo"
+idade = 15
+atividades_ja_efetuadas = 38
+ativo = True
+
+#Escritas e Corpo
+print("Olá estudante do Senac!")
+print("Bom dia!")
+print("Seu usuário é: "+nome)
+print(idade)
+print(atividades_ja_efetuadas)
+print(ativo)
+print(type(nome))
